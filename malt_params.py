80de7e31574a47d5a583c0467c0453715bb3d68b
jfoster17 <jonathan.bruce.foster@gmail.com>
2014-01-25 08:37:47 -0500

    Update params file

--- malt_params.py
+++ malt_params.py
@@ -1,38 +1,31 @@
 #This file stores variables for use
 #throughout the MALT90 reduction pipeline
+#Updated for remaking moment maps on Butch
+#where not all the pipeline will work.
 
-#moment_map
-#base_data = "/DATA/MALT_1/MALT90/data/"
 #Redundant with data_dir
 
-base = '/DATA/MALT_1/MALT90/'
+base = '/Volumes/Scratch/malt90'
 
 #reduce_malt
-#vnum = "1.6"
-vnum = {"rename":"1.5","ldata":"1.6","gzilla":"1.6","arrange":"1.6","mommaps":"1.6"}
+vnum = {"rename":"1.5","ldata":"1.6","gzilla":"1.6","arrange":"1.6","mommaps":"1.7"}
 #sd = '/nfs/atapplic/malt/reduce/'
 sd = '/epp/atapplic/malt/malt90-analysis-code/reduce/' #Seems to be new location
-data_dir = base+'data/'
-data_dir_y1 = base+'data/'+'year1/'
-data_dir_y2 = base+'data/'+'year2/'
+data_dir = base+
 
 #ReduceLog
 log_location = base+'reduction_log.txt'
 lock    = base+'lock.txt'
 source_dir = base+'raw_data/' #This is rather badly named
-#classification_file = sd+'Malt90Catalog_classifications_v16.txt'
-#classification_file = sd+'MALT90_classifications_2011.txt'
-classification_file = sd+'MALT90_classifications_2012.txt'
+#classification_file = sd+'MALT90_classifications_2012.txt'
 
 #preprocess
 rename_dir = '/DATA/MALT_1/MALT90/data/renamed/'
 #data_dir = '/DATA/MALT_1/MALT90/raw_data/'
 
-#examine_cal
-
-#data_dir = "/DATA/MALT_1/MALT90/cal/"
+#cal
 cal_dir = base+"cal/"
-ver_dir = base+"data/verification/"
+ver_dir = base+"results/verification/"
 
 #These are paths used by do_by_hand.py
 byhand_rename_dir = base+'data/byhand/renamed/'
